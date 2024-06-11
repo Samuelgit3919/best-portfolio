d9351dd60e8ce3369c4c341d80b40be2dc008d5c
sami <sami@sami.sami>
2024-06-11 15:59:53 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- for sign up page -->
+<?php 
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirmPassword = $_POST["confirmPassword"];
+
+
+//   db connection
+
+
+  
+?>
